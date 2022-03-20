76a3f4a5ea19b86f2bdd5cb8ed197c3dbcdf8cdf
tallua <talluay@gmail.com>
2022-03-20 09:21:54 +0000

    ui update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Container, Grid, IconButton, ImageList, ImageListItem, List, ListItem, ListItemText } from '@mui/material';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import UndoIcon from '@mui/icons-material/Undo';
+import { Box } from '@mui/system';
+
+
+const MAX_SEQUENCE = 13;
+
+const alphabets = [
+  'a', 'b', 'c', 'd', 'e',
+  'f', 'g', 'h', 'i', 'j',
+  'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y',
+  'z'
+];
 
 function App() {
+  let [sequence, setSequence] = useState<string[]>([]);
+  let [anagrams,] = useState<string[]>(Array(26).fill('empty'));
+
+  let filledSequence = sequence.concat(Array(MAX_SEQUENCE - sequence.length).fill('empty'));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container maxWidth="md">
+          <Box>
+            <ImageList
+              cols={alphabets.length}
+              gap={1}>
+              {
+                alphabets.map((ch, index) =>
+                  <ImageListItem
+                    key={`${index}:${ch}`}>
+                    <img
+                      src={`letters/${ch}.png`}
+                      alt={ch} />
+                  </ImageListItem>
+                )
+              }
+            </ImageList>
+          </Box>
+          <Box>
+            <ImageList
+              cols={MAX_SEQUENCE}
+              gap={1}>
+              {
+                filledSequence.map((ch, index) =>
+                  <ImageListItem
+                    key={`${index}:${ch}`}>
+                    <img
+                      src={`letters/${ch}.png`}
+                      alt={ch} />
+                  </ImageListItem>
+                )
+              }
+            </ImageList>
+            <Box>
+              <IconButton>
+                <UndoIcon color='info' />
+              </IconButton>
+              <IconButton>
+                <RestartAltIcon color='info' />
+              </IconButton>
+            </Box>
+          </Box>
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <List>
+                {
+                  anagrams.slice(0, 13).map((str) =>
+                    <ListItem key={str}>
+                      <ListItemText> str </ListItemText>
+                    </ListItem>
+                  )
+                }
+              </List>
+            </Grid>
+            <Grid item xs={6}>
+              <List>
+                {
+                  anagrams.slice(13, 26).map((str) =>
+                    <ListItem key={str}>
+                      <ListItemText> str </ListItemText>
+                    </ListItem>
+                  )
+                }
+              </List>
+            </Grid>
+          </Grid>
+        </Container>
+
       </header>
     </div>
   );
